886ea19efb0e033ed82b06cab363fbca37255ae7
Lisa Ino <lisaino@LisanoMacBook.local>
2020-01-23 09:06:04 +0900

    create works page

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,6 +1,68 @@
 <template>
   <div class="works">
     <h2>Works</h2>
+      <div class="block">
+        <h3>Portfolio</h3>
+
+        <img src="../assets/portfolio.png" class="thumbnail">
+
+        <table align="center" class="info">
+          <tbody>
+            <tr>
+              <td>URL</td>
+              <td><a href="/">https://portfolio-xxx.herokuapp.com</a></td>
+            </tr>
+            <tr>
+              <td>使用した技術</td>
+              <td>
+                Laravel / HTML / Sass / jQuery
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="block">
+        <h3>RDD JAPAN</h3>
+
+        <img src="../assets/rdd.png" class="thumbnail">
+
+        <table align="center" class="info">
+          <tbody>
+            <tr>
+              <td>URL</td>
+              <td><a href="https://rddjapan.info/2019" target="_blank">https://rddjapan.info/2019</a></td>
+            </tr>
+            <tr>
+              <td>使用した技術</td>
+              <td>
+                  Wordpress / PHP / HTML / Sass / jQuery
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="block">
+        <h3>転職エージェント用Webアプリケーション</h3>
+
+        <img src="../assets/1717335.jpg" class="thumbnail">
+
+        <table align="center" class="info">
+          <tbody>
+            <tr>
+              <td>URL</td>
+              <td>非公開</td>
+            </tr>
+            <tr>
+              <td>使用した技術</td>
+              <td>
+                  Laravel / HTML / Sass / Vue.jp / Bulma
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
   </div>
 </template>
 <script>
